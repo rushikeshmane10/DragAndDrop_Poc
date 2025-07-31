bb6ebd65341c8d178446b3bdf447d6880ef14c7c
Rushikesh <rushikeshmane401@gmail.com>
2025-07-31 16:21:49 +0530

    Update App.jsx
    Added api key

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { CmxLoop } from "./pages/Loop/KanBanLoop";
 
 function App() {
+  # OpenAI
+let openai_api_key = "sk-1234567890abcdefghijklmnopqrstuv"
   return (
     <>
       <BrowserRouter>
